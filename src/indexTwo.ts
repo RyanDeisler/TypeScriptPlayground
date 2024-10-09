93621d6076632adc262e4d0a33c104ab6deacf44
Ryan Deisler <ryandeisler@mail.adelphi.edu>
2024-10-09 16:20:18 -0400

    Inheritance / overriding

--- src/indexTwo.ts
+++ src/indexTwo.ts
@@ -38,3 +38,51 @@ class AtmMachine {
 
 let sbiMachine = new AtmMachine("Ryan", 123, 400000.99);
 console.log(sbiMachine.checkBalance());
+
+//Inheritance
+class MainBranch {
+  accountNumber: number;
+  constructor(accountNumber: number) {
+    this.accountNumber = accountNumber;
+  }
+
+  createAccount(): void {
+    console.log("Account is created: ", this.accountNumber);
+  }
+
+  getCarLoan(): void {
+    console.log("We provide car loans with an interest rate of 12%");
+  }
+}
+
+class BranchOne extends MainBranch {
+  constructor(accountNumber: number) {
+    super(accountNumber);
+  }
+
+  getCarLoan(): void {
+    console.log("We provide car loans with an 11% interest rate");
+  }
+}
+
+class BranchTwo extends MainBranch {
+  interestRate: number;
+  getCarLoan(): void {
+    console.log("The car loan interest rate is: ", this.interestRate);
+  }
+  constructor(accountNumber: number, interestRate: number) {
+    super(accountNumber);
+    this.interestRate = interestRate;
+  }
+}
+
+let mainBranchCustomer = new MainBranch(532);
+mainBranchCustomer.getCarLoan();
+
+let newCustomer = new BranchOne(1234);
+newCustomer.createAccount();
+newCustomer.getCarLoan();
+
+let newCustomerBranchTwo = new BranchTwo(231, 4.4);
+newCustomerBranchTwo.createAccount();
+newCustomerBranchTwo.getCarLoan();
